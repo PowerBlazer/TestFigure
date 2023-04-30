4e16e9732385b44f978d826d825afd3b8ce0ce8c
Айнур Якупов <104465016+PowerBlazer@users.noreply.github.com>
2023-04-30 15:56:48 +0300

    Sql result

---
+++ ProductsCategory.sql
@@ -0,0 +1,45 @@
+CREATE TABLE Product (
+	Id INT PRIMARY KEY,
+	"Name" NVARCHAR(MAX)
+);
+
+CREATE TABLE Category (
+	Id INT PRIMARY KEY,
+	"Name" NVARCHAR(MAX)
+);
+
+CREATE TABLE ProductCategory (
+    Id INT PRIMARY KEY IDENTITY,
+	ProductId INT, 
+  	FOREIGN KEY (ProductId) REFERENCES Product(Id),
+	CategoryId INT, 
+  	FOREIGN KEY (CategoryId) REFERENCES Category(Id),
+);
+
+INSERT INTO Product
+VALUES
+	(1, N'Телевизор'),
+	(2, N'Яблоко'),
+	(3, N'Машина'),
+    (4, N'Ковер');
+    
+INSERT INTO Category
+VALUES
+	(1, N'Бытовая техника'),
+	(2, N'Продукты'),
+	(3, N'Транспорт');
+
+
+INSERT INTO ProductCategory
+VALUES
+	(1, 1),
+	(2, 2),
+	(3, 3);
+
+SELECT P."Name", C."Name"
+FROM Product P
+LEFT JOIN ProductCategory PC
+	ON P.Id = PC.ProductId
+LEFT JOIN Category C
+	ON PC.CategoryId = C.Id;
+
